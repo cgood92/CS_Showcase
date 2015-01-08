49a55082cb098093b79f57305c442efb894fc400
Clint Goodman <goodmancf@ldschurch.org>
2015-01-08 14:20:58 -0700

    Added an exception, a few logical checking items, and comments

--- Vector Class/Vector.h
+++ Vector Class/Vector.h
@@ -4,7 +4,11 @@ template <class T>
 class Vector
 {
 public:
-	Vector() : data(NULL), current_size(0), capacity_size(0) {} //Constructor
+	Vector() //Default constructor
+	{
+		Vector(0);
+	}
+	Vector(int capacity_size) : data(NULL), current_size(0), capacity_size(capacity_size) {} //Non-default constructor
 	~Vector()  //Destructor
 	{
 		data = NULL;
@@ -30,23 +34,37 @@ public:
 	}
 	void push_back(T item)
 	{
+		//Capacity has to be more than one
+		if (capacity_size == 0)
+		{
+			capacity_size = 1;
+		}
+		//Array has not been created yet
 		if (current_size == 0)
 		{
 			data = new T[capacity_size];
 		}
+		//If the capactiy is full, double the capactiy
 		if (current_size >= capacity_size)
 		{
 			capacity_size *= 2;
 			//TODO: make a new array; copy all the old array into this array
+			T * tempData = new T[capacity_size];
+			for (int i = 0; i < current_size; i++)
+			{
+				tempData[i] = data[i];
+			}
+			data = tempData;
 		}
+		//Add the data
 		data[current_size] = item;
 		current_size++;
 	}
-	T & operator [](int index)
+	T & operator [](int index) throw()
 	{
 		if (index < 0 || index >= capacity_size)
 		{
-			cout << "Excpetion thrown";
+			throw "ERROR: Invalid index";
 		}
 		return data[index];
 	}
