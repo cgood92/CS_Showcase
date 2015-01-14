7e9049f835411ffdf58763c741bb0ec19ab0edaf
Clint Goodman <goodmancf@ldschurch.org>
2015-01-13 18:51:24 -0700

    Setting up project for Set class and Go Fish

---
+++ Set Class/Set.h
@@ -0,0 +1,31 @@
+/***********************************************************************
+* Program:
+*    Lesson 01, Set
+*    Brother Sloan, CS 235
+* Author:
+*    Clint Goodman and Parker Hubbard
+* Summary:
+*    This is a header file that implements a data structure acting
+*	just like a Set.  
+************************************************************************/
+
+#include <iostream>       // for CIN and COUT
+
+// declare prototype
+template <class T>
+class SetIterator;
+
+
+template <class T>
+class Set
+{
+public:
+	
+};
+
+template <class T>
+class SetIterator
+{
+public:
+
+};
---
+++ Set Class/goFish.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************
+* Program:
+*    Assignment 01, Go Fish
+*    Brother Sloan, CS 235
+* Author:
+*     Clint Goodman and Parker Hubbard
+* Summary: 
+*    This is all the functions necessary to play Go Fish!
+*
+*    Estimated:  0.0 hrs   
+*    Actual:     0.0 hrs
+*      Please describe briefly what was the most difficult part.
+************************************************************************/
+
+#include <iostream>
+#include "set.h"
+#include "card.h"
+#include "goFish.h"
+using namespace std;
+
+/**********************************************************************
+ * GO FISH
+ * The function which starts it all
+ ***********************************************************************/
+void goFish()
+{
+   return ;
+}
