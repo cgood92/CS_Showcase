2a15a17409d0cacde135df2cd5f4114d9d9ab0fd
Clint Goodman <goodmancf@ldschurch.org>
2015-01-23 15:37:14 -0700

    Parker's edits to stack.h, plus my infix solution

--- Stack Class/Stack Class/Stack Class/infix.cpp
+++ Stack Class/Stack Class/Stack Class/infix.cpp
@@ -19,9 +19,116 @@ using namespace std;
  * CONVERT INFIX TO POSTFIX
  * Convert infix equation "5 + 2" into postifx "5 2 +"
  *****************************************************/
+/*
+In addition to passing the four test functions for the Stack class, you will also need to use the Stack class to implement a program to convert an infix expression (4 + 8 * 3) into a postfix expression (4 8 3 * +). There is a discussion on this in chapter 7.5 of the textbook. Note that you will need to handle the following operators:
+
+() The parentheses are the highest level of the order of operations.
+^ The exponent operator comes immediately after the parentheses. Note that the algorithm in the textbook cannot handle the exponent operator properly. You will need to handle this case.
+* / % Multiplication, division, and modulus are after the exponent operator. They are at the same level of the order of operations.
++ - Addition and subtraction are after multiplication, division, and modulus.
+
+*/
 string convertInfixToPostfix(const string & infix)
 {
    string postfix;
+   Stack<char> operators;
+
+   bool notAnOperator;
+   char c;
+   for (std::string::size_type i = 0; i < infix.size(); ++i)
+   {
+	   c = infix[i];
+	   notAnOperator = false;
+	   
+	   //Check for operators
+	   switch (c)
+	   {
+	   case '(':
+		   operators.push(c);
+		   break;
+	   case ')':
+		   for (char oper = operators.top(); !operators.empty() && oper != '('; operators.pop(), oper = operators.top())
+		   {
+			   postfix += ' ';
+			   postfix += oper;
+		   }
+		   operators.pop(); // Get rid of left paranthesis
+		   break;
+	   case '^':
+		   operators.push(c);
+		   break;
+	   case '*':
+	   case '/':
+	   case '%':
+		   if (operators.empty() || operators.top() == '(')
+		   {
+			   operators.push(c);
+		   }
+		   else if (operators.top() == '+' || operators.top() == '-') //Greater precedence
+		   {
+			   operators.push(c);
+		   }
+		   else if (operators.top() == '*' || operators.top() == '*' || operators.top() == '/')
+		   {
+			   postfix += ' ';
+			   postfix += operators.top();
+			   operators.pop();
+			   operators.push(c);
+		   }
+		   else //Less precedence
+		   {
+			   for (char oper = operators.top(); !operators.empty() && oper == '^'; operators.pop())
+			   {
+				   postfix += ' ';
+				   postfix += oper;
+			   }
+		   }
+		   break;
+		case '+':
+		case '-':
+			if (operators.empty() || operators.top() == '(') //Empty or equal precedence
+			{
+				operators.push(c);
+			}
+			else if (operators.top() == '+' || operators.top() == '-')
+			{
+				postfix += ' ';
+				postfix += operators.top();
+				operators.pop();
+				operators.push(c);
+			}
+			else //Lowest precedence
+			{
+				for (char oper = operators.top(); !operators.empty() && (oper == '^' || oper == '*' || oper == '/' || oper == '%'); operators.pop())
+				{
+					postfix += ' ';
+					postfix += operators.top();
+				}
+				operators.push(c);
+			}
+		   break;
+		default:
+			notAnOperator = true;
+			break;
+	   }
+
+	   //Skip if it was an operator (performance)
+	   if (!notAnOperator)
+		   continue;
+
+	   postfix += c;
+   }
+
+   if (!operators.empty())
+   {
+	   for (; !operators.empty(); operators.pop())
+	   {
+		   postfix += ' ';
+		   postfix += operators.top();
+	   }
+   }
+
+   //Print out the operators
 
    return postfix;
 }
@@ -69,6 +176,7 @@ void testInfixToPostfix()
 string convertPostfixToAssembly(const string & postfix)
 {
    string assembly;
+
 
    return assembly;
 }
--- Stack Class/Stack Class/Stack Class/stack.h
+++ Stack Class/Stack Class/Stack Class/stack.h
@@ -1,142 +1,195 @@
-﻿/***********************************************************************
-* Program:
-*    Lesson 02, Stacks
-*    Brother Sloan, CS 235
-* Author:
-*	Clint Goodman and Parker Hubbard
-* Summary:
-************************************************************************/
-
+/***********************************************************************
+ * Program:
+ *    Lesson 0s, Infix to Postfix
+ *    Brother Sloan, CS 235
+ * Author:
+ *      Clint Goodman and Parker Hubbard
+ * Summary:
+ ************************************************************************/
 #ifndef STACK_H
 #define STACK_H
-#include <cassert>
 
-//#include <vector>
+#include <cassert>
 #include <iostream>
 #include <string>
 using namespace std;
 
-
 /**********************************************************************
-* Stack
-* //creating the class for stacks
-***********************************************************************/
+ * Stack
+ * //creating the class for Stacks
+ ***********************************************************************/
 template <class T>
 class Stack
 {
-private:
-	T * data;         
-	int current_size; 
-	int capacity_size;
-	void resize(int newCapacity) throw (bad_alloc);
+  private:
+   T * data;          //dynamically allocated array of T
+   int current_size;  //the current Set size, or number of items
+   int capacity_size; //how many items can I put on the Set before full?
+   int topSpot;       //the "X" value of where the top item is found
+   void resize(int newCapacity) throw (bad_alloc);
+  public:
+   /////////CONSTRUCTORS///////////////////
+   // default constructor : empty and kinda useless
+  Stack() : data(NULL), current_size(0), capacity_size(0), topSpot(-1) {}
+   // copy constructor : copy it
+  Stack(const Stack<T> & rhs) : data(NULL), current_size(0), capacity_size(0), topSpot(-1)
+   {
+      if (rhs.size())   { *this = rhs; }
+   }
+   // non-default constructor : pre-allocate
+  Stack(int capacity) : data(NULL), current_size(0), capacity_size(0), topSpot(-1)
+   {
+      if (capacity > 0) { resize(capacity); }
+   }
+   // destructor : free everything that was previously used
+   ~Stack()
+   {
+      if (NULL != data)//(capacity)
+      {  //assert(capacity);
+         delete[] data;
+      }
+   }
 
-public:
-	Stack() : data(NULL), current_size(0), capacity_size(0) {}
-	Stack(int capacity) : data(NULL), current_size(0), capacity_size(0)
-	{
-		if (capacity > 0) { resize(capacity); }
-	}
-	Stack(const Stack<T> & rhs) : data(NULL), current_size(0), capacity_size(0)
-	{
-		if (rhs.size())   { *this = rhs; }
-	}
-	~Stack()
-	{
-		if (NULL != data)
-		{
-			delete[] data;
-		}
-	}
+   /////////COMMANDS///////////////////
+   //size(): Return the Setsize
+   int size()        const{ return current_size; }
+   //empty(): Test whether the set is empty
+   bool empty()      //const { return current_size == 0; }
+      const { return topSpot == -1; }
 
-	int size()        const{ return current_size; }
-	bool empty()      const { return current_size == 0; }
-	T & top() throw(const char *);
-	//TODO
-	void pop() {}
-	void push(const T & item);
+   //push(): Adds an item to the container.
+   void push(const T & item);
+   //top(): Returns the item currently at the top of the stack.
+   T & top() throw(const char *);
 
-	Stack<T> & operator = (const Stack<T> & rhs) throw (bad_alloc);
+   //pop(): Removes an item from the end of the stack,
+   //serving to reduce the size by one.
+   void pop() throw(const char *)
+   {
+      //if the stack is already empty, the
+      //following c-string exception will be thrown:
+      if (!empty())
+      {
+         topSpot = topSpot - 1;
+         current_size = current_size - 1;
+      }
+      else
+      {
+         throw "ERROR: Unable to pop from an empty Stack";
+      }
+   }
+
+   Stack<T> & operator = (const Stack<T> & rhs) throw (bad_alloc);
 };
 
-/**********************************************************************
-* Stack-push
-* //creating the steps for inserting a unit/value to the last spot
-***********************************************************************/
+//push(): Adds an item to the container.
 template <class T>
 void Stack<T> ::push(const T & item)
 {
-	if (current_size >= capacity_size)
-	{
-		if (capacity_size == 0)
-		{
-			capacity_size = 1;
-		}
-		resize(capacity_size * 2);
-	}
-	//Add the data
-	data[current_size] = item;
-	current_size++;
+   //Note that if the stack is full, then the capacity will be doubled.
+   //if (current_size >= capacity_size)
+   //{
+   //   if (capacity_size == 0)
+   //   {
+   //      capacity_size = 1;
+   //   }
+   //   resize(capacity_size * 2);
+   //}
+   //Add the data
+   //data[current_size] = item;
+   //current_size++;
+   //topSpot = topSpot + 1;
+
+   if (topSpot <= (capacity_size - 1))
+   {
+      if (capacity_size == 0)
+      {
+         capacity_size = 1;
+      }
+      resize(capacity_size * 2);
+      current_size++;
+      topSpot = topSpot + 1;
+      data[topSpot] = item;
+   }
+   else
+   {}
 }
 
 /**********************************************************************
-* Stack-top
-***********************************************************************/
+ * Stack-top
+ *  -Returns the item currently at the top of the stack.
+ ***********************************************************************/
 template <class T>
-T & Stack<T> ::top() throw(const char *)
+T & Stack<T> ::top()throw(const char *)
 {
-	if (current_size == 0)
-	{
-		throw "ERROR: Unable to reference the element from an empty Stack";
-	}
-	return data[current_size];
+   //   cout << "hi";
+   if (!empty())
+   {
+      return data[topSpot];
+      cout << "hi2";
+   }
+   else
+   {
+      throw "ERROR: Unable to reference the element from an empty Stack";
+   }
 }
 
+//template <class T>
+//T & Stack<T> ::top() throw(const char *)
+//{
+//   if (!empty())
+//   {
+//      return data[current_size];
+//   }
+//   else
+//   {
+//      throw "ERROR: Unable to reference the element from an empty Stack";
+//   }
+//}
 
 /**********************************************************************
-* //creating the steps for resizing the Stack
-***********************************************************************/
+ * //creating the steps for resizing the Stack
+ ***********************************************************************/
 template <class T>
 void Stack<T> ::resize(int newCapacity) throw (bad_alloc)
 {
-	//allocate new array
-	T * pNew;
-	pNew = new T[newCapacity];
-	//copy data from old array
-	for (int i = 0; i < current_size; i++)
-	{
-		pNew[i] = data[i];
-	}
-	//delete old and assign the new
-	delete[] data;
-	data = pNew;
-	capacity_size = newCapacity;
+   //allocate new array
+   T * pNew;
+   pNew = new T[newCapacity];
+   //copy data from old array
+   for (int i = 0; i < current_size; i++)
+   {
+      pNew[i] = data[i];
+   }
+   //delete old and assign the new
+   delete[] data;
+   data = pNew;
+   capacity_size = newCapacity;
 }
 
 /**********************************************************************
-* Stack-operator equals
-* //creating the steps for equaling the Stack
-***********************************************************************/
+ * Stack-operator equals
+ * //creating the steps for equaling the Stack
+ ***********************************************************************/
 template <class T>
 Stack<T> & Stack<T> :: operator = (const Stack<T> & rhs) throw (bad_alloc)
 {
-	current_size = 0; 
-	if (rhs.current_size > capacity_size)
-	{
-		resize(rhs.current_size);
-	}
-	assert(capacity_size >= rhs.current_size);
+   current_size = 0;
+   if (rhs.current_size > capacity_size)
+   {
+      resize(rhs.current_size);
+   }
+   assert(capacity_size >= rhs.current_size);
 
-	//copy the data from the other size
-	current_size = rhs.current_size;
-	for (int i = 0; i < rhs.current_size; i++)
-	{
-		data[i] = rhs.data[i];
-	}
-	//return itself
-	return *this;
+
+   //copy the data from the other size
+   current_size = rhs.current_size;
+   for (int i = 0; i < rhs.current_size; i++)
+   {
+      data[i] = rhs.data[i];
+   }
+   //return itself
+   return *this;
 }
-
-
 #endif
-
-
+   
