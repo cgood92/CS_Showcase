c638a9e25e3bf17aae69819a5bfa7395561b5c3f
Clint Goodman <goodmancf@ldschurch.org>
2015-01-29 11:08:15 -0700

    Completing the queue

--- Queue Class/Queue Class/queue.h
+++ Queue Class/Queue Class/queue.h
@@ -23,21 +23,25 @@ class Queue
 {
   private:
      T * data;      //dynamically allocated array of T
-     int frontSpot; //the "X" value of where the bottum/front item is found
+     int frontSpot; //the "X" value of where the bottom/front item is found
      int backSpot;  //the "X" value of where the top/back item is found
      int current_size;  //the current Set size, or number of items
      int capacity_size; //how many items can I put on the Set before full?
      void resize(int newCapacity) throw (bad_alloc);
   public:
+     ////////////CLASS CONSTRUCTORS///////////////////
      // default constructor :
-     Queue() : data(NULL) {}
+     Queue() : data(NULL), current_size(0), capacity_size(0),
+        frontSpot(-1), backSpot(-1){}
      // copy constructor : copy details of another indicated
-  Queue(const Queue<T> & rhs) : data(NULL), current_size(0), capacity_size(0), frontSpot(-1), backSpot(-1)
+     Queue(const Queue<T> & rhs) : data(NULL), current_size(0),
+        capacity_size(0), frontSpot(-1), backSpot(-1)
      {
         if (rhs.size())   { *this = rhs; }
      }
      // non-default constructor : pre-allocate
-  Queue(int capacity) : data(NULL), current_size(0), capacity_size(0), frontSpot(-1), backSpot(-1)
+     Queue(int capacity) : data(NULL), current_size(0),
+        capacity_size(0), frontSpot(-1), backSpot(-1)
      {
         if (capacity > 0) { resize(capacity); }
      }
@@ -51,11 +55,149 @@ class Queue
      }
      
      /////////COMMANDS///////////////////
-     //size(): Return the Setsize
-     int size()        const{ return current_size; }
      //empty(): Test whether the set is empty
-     bool empty()      //const { return current_size == 0; }
-     //POSSIBLY INDICATE EMPTY WHEN FRONT_SPOT == BACK_SPOT
-        const { return backSpot == -1; }
+     bool empty() const { return backSpot == frontSpot; }
+     //size:    Return the current_size of the Queue
+     int size()        const{ return current_size; }
+     //capacity:Return the capacity_size of the Queue
+     int capacity()    const{ return capacity_size; }
+     
+     Queue<T> & operator = (const Queue<T> & rhs) throw (bad_alloc);
+
+     //push():  Adds an item to the top/back of the container.
+     void push(const T & item);
+     //back():  Returns the item currently at the top/back of the stack.
+     T & back()   throw(const char *);
+     //front(): Returns the item currently at the bottom/front of the stack.
+     T & front()  throw(const char *);
+     //pop(): Removes an item from the front/bottom of the stack
+     void pop()   throw(const char *);
+     
 };
+/**********************************************************************
+ * (1)//creating the steps for resizing the Queue
+ ***********************************************************************/
+template <class T>
+void Queue<T> ::resize(int newCapacity) throw (bad_alloc)
+{
+   //allocate new array
+   T * pNew;
+   pNew = new T[newCapacity];
+   //copy data from old array
+   for (int i = 0; current_size > 0 && i < capacity_size; i++)
+   {
+      pNew[i] = data[i];
+   }
+   //delete old and assign the new
+   delete[] data;
+   data = pNew;
+   capacity_size = newCapacity;
+}
+
+/**********************************************************************
+ * (2)Stack-push
+ *  -Adds an item to the top of the stack.
+ ***********************************************************************/
+template <class T>
+void Queue<T> ::push(const T & item)
+{
+   //Note that if the stack is full, then the capacity will be doubled.
+   if (backSpot == (capacity_size - 1))
+   {
+      if (capacity_size == 0)
+      {
+         capacity_size = 1;
+      }
+      resize(capacity_size * 2);
+      //add 1 to current number of items/positions, then place an item
+      //at that spot
+      current_size++;
+      data[++backSpot] = item;
+   }
+   else
+   {
+      current_size++;
+      data[++backSpot] = item;
+   }
+}
+
+/**********************************************************************
+ * (3)Queue-front
+ *  -Returns the item currently at the back/top of the stack.
+ ***********************************************************************/
+template <class T>
+T & Queue<T> ::back()throw(const char *)
+{
+   if (!empty())
+   {
+      return data[backSpot];
+   }
+   else
+   {
+      throw "ERROR: attempting to access an item in an empty queue";
+   }
+}
+
+/**********************************************************************
+ * (4)Queue-front
+ *  -Returns the item currently at the front/bottom of the Queue.
+ ***********************************************************************/
+template <class T>
+T & Queue<T> ::front()throw(const char *)
+{
+   if (!empty())
+   {
+      return data[frontSpot+1];
+   }
+   else
+   {
+	   throw "ERROR: attempting to access an item in an empty queue";
+   }
+}
+
+/**********************************************************************
+ * (5)Stack-pop
+ *  -Removes the item from the head
+ ***********************************************************************/
+template <class T>
+void Queue<T> ::pop() throw(const char *)
+{
+   if (!empty())
+   {
+      frontSpot++;
+      current_size--;
+   }
+   else
+   {
+      //if the stack is already empty, the
+      //following c-string exception will be thrown:
+	   throw "ERROR: attempting to pop from an empty queue";
+   }
+}
+
+/**********************************************************************
+ * Queue-operator equals
+ * //creating the steps for equaling the Queue
+ ***********************************************************************/
+template <class T>
+Queue<T> & Queue<T> :: operator = (const Queue<T> & rhs) throw (bad_alloc)
+{
+   current_size = 0;
+   if (rhs.current_size > capacity_size)
+   {
+      resize(rhs.current_size);
+   }
+   assert(capacity_size >= rhs.current_size);
+
+
+   //copy the data from the other size
+   current_size = rhs.current_size;
+   for (int i = 0; i < rhs.current_size; i++)
+   {
+      data[i] = rhs.data[i];
+   }
+   //return itself
+   return *this;
+}
+   
 #endif
