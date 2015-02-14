bd5217ed3aae77b8fb05efb9df8af50f27380662
Clint Goodman <goodmancf@ldschurch.org>
2015-02-13 22:23:59 -0700

    Names added

--- Linked-List/Linked-List/sortInsertion.h
+++ Linked-List/Linked-List/sortInsertion.h
@@ -5,7 +5,7 @@
  *    This will contain just the prototype for insertionSortTest(). You may
  *    want to put other class definitions here as well.
  * Author
- *    <your names here>
+ *    Clint Goodman and Parker Hubbard
  ************************************************************************/
 
 #ifndef INSERTION_SORT_H
