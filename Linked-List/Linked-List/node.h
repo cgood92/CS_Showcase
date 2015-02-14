d11a61c6dde46011c6d6dead15dd3e007058466c
Clint Goodman <goodmancf@ldschurch.org>
2015-02-13 20:35:32 -0700

    Accepting Parker's text formatting and comments, built sort methodology using our linked list

--- Linked-List/Linked-List/node.h
+++ Linked-List/Linked-List/node.h
@@ -25,20 +25,18 @@ class Node
   public:
      T data;
      Node <T> * pNext;
-	 Node() : data(NULL), pNext(NULL)
-	 {
-	 }
-	 
-	 // Non-default
-	 Node(T headData) : data(headData), pNext(NULL)
-	 {
-	 }
-
+     //default constructor
+     Node() : data(NULL), pNext(NULL)
+     {
+     }	 
+     // Non-default
+     Node(T headData) : data(headData), pNext(NULL)
+     {
+     }
      // copy constructor
      Node(const Node<T> & rhs)
      {
      }
-     
      // destructor : free everything that was previously used
      ~Node()
      {
@@ -56,23 +54,38 @@ class Node
 template <class T>
 Node <T> * copy(Node <T> * rhs)
 {
+   //a node pointer "current" points to a new node
    Node <T> * current = new Node<T>();
-   Node <T> * pHead = current;
+   //a node pointer "pHead" points to where "current" points, currently
+   //the new node
+   Node <T> * pHead = current; 
+   //pointer "p" points to where pointer "rhs" is at. continues while "p"
+   //is true, or doesnt equal NULL. "p" now points to where "p's" current
+   //pNext is pointing to
    for (Node <T> * p = rhs; p; p = p->pNext)
    {
+   //"prev" points to where "current" currently points
       Node <T> * prev = current;
+   //the value/data at "prev" equals the value/data at "p"
       prev->data = p->data;
+   //if the pointer the "pNext" that "p" points to doesnt equal NULL...
       if (p->pNext != NULL)
       {
+      //the pointer "next" points to a new node
          Node <T> * next = new Node<T>();
+      //the node pointer "pNext" that "prev" is pointing to, now points to
+      //the node "next" is pointing to
          prev->pNext = next;
+      //the pointer "current" points currently to what "next" is pointing to
          current = next;
       }
       else
       {
+         //the pointer "pNext" that "prev" is pointing to, points/equals NULL 
          prev->pNext = NULL;
       }
    }
+   //return what pointer "pHead" is pointing to
    return pHead;
 }
 
@@ -84,26 +97,10 @@ Node <T> * copy(Node <T> * rhs)
 template <class T>
 void freeData(Node <T> * & rhs)
 {
-	rhs = NULL;
-	delete rhs;
-	/*
-	Node <T> * current = new Node<T>();
-	Node <T> * pHead = current;
-	for (Node <T> * p = rhs; p; p = p->pNext)
-	{
-		Node <T> * prev = current;
-		prev->data = NULL;
-		if (p->pNext != NULL)
-		{
-			Node <T> * next = new Node<T>();
-			prev->pNext = NULL;
-			delete prev;
-			current = next;
-		}
-		p = NULL;
-		delete p;
-	}
-	*/
+   //We'll need to imrpvoe this function later to actually
+   //release all memory
+   rhs = NULL; 
+   delete rhs;
 }
 
 /**********************************************************************
@@ -118,49 +115,70 @@ void freeData(Node <T> * & rhs)
 template <class T>
 void insert(const T & data, Node<T> * & pPrev)
 {
-	// 1. allocate and initialize create a new nod
-	Node<T> * pNew = new Node<T>(data);   // allocate the node
-
+   // 1. allocate and initialize create a new node
+   Node<T> * pNew = new Node<T>(data);   // allocate the node
    if (pPrev != NULL)
    {
-	   // 2. fix the pNext pointer from pNew to the spot after it in the list
-	   pNew->pNext = pPrev->pNext;
+      // 2. fix the pNext pointer from pNew to the spot after it in the list
+      pNew->pNext = pPrev->pNext;
 
-	   // 3. finally get pNode->pNext to point to pNew instead of the old node
-	   pPrev->pNext = pNew;
+      // 3. finally get pNode->pNext to point to pNew instead of the old node
+      pPrev->pNext = pNew;
    }
    //New linked list - empty
    else
    {
-	   pPrev = pNew;
+      pPrev = pNew;
    }
 }
-
+/////////////////Exact same, including a bool parameter as well////////////
 template <class T>
 void insert(const T & data, Node<T> * & pHead, bool head)
 {
-	if (head)
-	{
-		// 1. allocate and initialize create a new node
-		Node<T> * pNew = new Node<T>(data);               // allocate the node
+   if (head)
+   {
+      // 1. allocate and initialize create a new node
+      Node<T> * pNew = new Node<T>(data);   // allocate the node
 
-		if (pHead != NULL)
-		{
-			// 2. put pHead in line after the new node
-			pNew->pNext = pHead;                  // set pNext, the old head of the list
+      if (pHead != NULL)
+      {
+         // 2. put pHead in line after the new node
+         pNew->pNext = pHead;          // set pNext, the old head of the list
 
-			// 3. finally, pNew is the new head
-			pHead = pNew;                         // pNew is the new head.  Note that
-		}
-		else
-		{
-			pHead = pNew;
-		}
-	}
-	else
-	{
-		insert(data, pHead);
-	}
+         // 3. finally, pNew is the new head
+         pHead = pNew;                 // pNew is the new head.  Note that
+      }
+      else
+      {
+         pHead = pNew;
+      }
+   }
+   else
+   {
+      insert(data, pHead);
+   }
+}
+
+/**********************************************
+ * FIND NODE
+ * Looking for a node based on the index.  Note
+ * that the index is 1's based.  If there are
+ * six items, then the valid indicies are 1..6.
+ * Return pHead if no node exists there
+ **********************************************/
+template <class T>
+Node<T> * find(Node<T> * & pHead, const T & data)
+{
+   // search through the linked list
+   for (Node<T> * p = pHead; p; p = p->pNext)
+   {
+      if (p->data == data)
+      {
+         return p;
+      }
+   }
+
+   return NULL;        // return pHead if no node exists
 }
 
 /*****************************************
@@ -170,39 +188,15 @@ void insert(const T & data, Node<T> * & pHead, bool head)
 template <class T>
 ostream & operator << (ostream & out, Node<T> * pHead)
 {
-	for (Node <T> * p = pHead; p; p = p->pNext)
-	{
-		out << p->data;
-		if (p->pNext)
-		{
-			out << ", ";
-		}
-	}
+   for (Node <T> * p = pHead; p; p = p->pNext)
+   {
+      out << p->data;
+      if (p->pNext)
+      {
+         out << ", ";
+      }
+   }
    return out;
 }
-
-/**********************************************
-* FIND NODE
-* Looking for a node based on the index.  Note
-* that the index is 1's based.  If there are
-* six items, then the valid indicies are 1..6.
-* Return pHead if no node exists there
-**********************************************/
-template <class T>
-Node<T> * find(Node<T> * & pHead, const T & data)
-{
-	// search through the linked list
-	for (Node<T> * p = pHead; p; p = p->pNext)
-	{
-		if (p->data == data)
-		{
-			return p;
-		}
-	}
-
-	return NULL;                         // return pHead if no node exists
-}
-
-
 
 #endif
--- Linked-List/Linked-List/sortInsertion.h
+++ Linked-List/Linked-List/sortInsertion.h
@@ -19,7 +19,47 @@
  **********************************************/
 template <class T>
 void sortInsertion(T array[], int num)
-{
+{   
+   Node <T> * n = NULL;
+/***********************************************
+ * 1) add all items unto the list from the array (back to front)
+ **********************************************/
+   for (int i = num - 1; i >= 0; i--)
+   {
+	   //first time throug - no need to sort, just put in front
+	   if (i == num - 1)
+	   {
+		   insert(array[i], n, true);
+		   continue;
+	   }
+	   //Check to see if this value is less than something in the array (if so, place it where it fits)
+	   for (Node <T> * ph = n; ph; ph = ph->pNext)
+	   {
+		   //If this is going to replace the head...
+		   if (ph->data == n->data && array[i] < ph->data)
+		   {
+			   insert(array[i], n, true);
+			   break;
+		   }
+		   //If this is next up, or this is the end of the list ...
+		   if (ph->pNext && array[i] < ph->pNext->data || !ph->pNext)
+		   {
+			   insert(array[i], ph);
+			   break;
+		   }
+	   }
+   }
+
+/***********************************************
+ * add all items of linked list (possibly sorted)
+ * to the previous array
+ **********************************************/
+   int jj = 0;
+   for (Node <T> * ph = n; ph; ph = ph->pNext)
+   {
+      array[jj] = ph->data;
+      jj = jj + 1;
+   }
 }
 
 #endif // INSERTION_SORT_H
