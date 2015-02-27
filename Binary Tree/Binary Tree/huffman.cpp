319e4a10e5cf1e5ac220f8d57f0b0e2d6e9d3d64
Clint Goodman <goodmancf@ldschurch.org>
2015-02-27 12:15:24 -0700

    Getting initial stuff setup

--- Binary Tree/Binary Tree/huffman.cpp
+++ Binary Tree/Binary Tree/huffman.cpp
@@ -9,12 +9,54 @@
  ************************************************************************/
 
 #include "huffman.h"       // for HUFFMAN() prototype
+#include "pair.h"
+#include "vector.h"
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "bnode.h"     // your BinaryNode class should be in bnode.h
 
+#include <fstream>         // needed for all file I/O programs
+using namespace std;
 /*******************************************
  * HUFFMAN
  * Driver program to exercise the huffman generation code
  *******************************************/
+//template <typename T>
 void huffman()
 {
+   char fileName[256];
+   //create the tree
+   Vector <Pair<string, float>> huffCodes;
+
+   //get the filename
+   cout << "Enter the filename containing the value frequencies: ";
+   cin >> fileName;
+
+   string line;
+   ifstream myfile("C:\\Users\\cgood92\\Documents\\CS 235\\Binary Tree\\Binary Tree\\huffman1.txt");
+   if (myfile.is_open())
+   {
+	   while (getline(myfile, line))
+	   {
+		   Pair<string, float> thisPair; //String: token, float: frequency
+		   istringstream(line) >> thisPair;
+		   huffCodes.push_back(thisPair);
+	   }
+	   myfile.close();
+   }
+   else
+   {
+	   cout << "Error opening file";
+   }
+
+   VectorIterator <Pair<string, float>> it;
+   for (it = huffCodes.begin(); it != huffCodes.end(); ++it)
+	   cout << "\t" << *it << endl;
+   
    return;
 }
+
+
--- Binary Tree/Binary Tree/huffman.h
+++ Binary Tree/Binary Tree/huffman.h
@@ -10,7 +10,22 @@
 
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
+#include "pair.h"
+#include "bnode.h"
+#include <cassert>
+#include <iostream>
+#include <string>
 
+/**********************************************************************
+ * The HuffmanTree
+ ***********************************************************************/
+class HuffmanTree
+{
+  private:
+  public:
+	   BinaryNode < Pair<string, float> >  miniTree;
+};
 void huffman();
 
 #endif // HUFFMAN_h
+	
