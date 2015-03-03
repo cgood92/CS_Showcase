fe96875fccfdd55b2a31d27beee87363811be1cd
Clint Goodman <goodmancf@ldschurch.org>
2015-03-02 22:37:25 -0700

    Stubbing it out

--- Binary Search Tree/Binary Search Tree/bst.h
+++ Binary Search Tree/Binary Search Tree/bst.h
@@ -12,6 +12,7 @@
 #include <cassert>
 #include <iostream>
 #include <string>
+#include "bnode.h"
 using namespace std;
 
 
@@ -22,6 +23,80 @@ template <class T>
 class BST
 {
   private:
+	  BinaryNode * masterTree;
   public:
+	  BST() {}
+	  ~BST() {}
+	  BST(const BST & rhs) {}
+	  bool empty();
+	  void insert() throw(const char *);
+	  void remove();
+	  void clear();
+	  BSTIterator <T> & find();
+	  BSTIterator <T> & begin();
+	  BSTIterator <T> & end();
+	  BSTIterator <T> & rbegin();
+	  BSTIterator <T> & rend();
 };
+
+
+
+/**********************************************************************
+* BSTIterator
+***********************************************************************/
+template <class T>
+class BSTIterator
+{
+private:
+	BinaryNode * masterTree;
+public:
+	BSTIterator() {}
+	BSTIterator(const BSTIterator & rhs)
+	{
+		*this = rhs;
+	}
+	BSTIterator & operator = (const BSTIterator & rhs)
+	{
+		this->masterTree = rhs.masterTree;
+		return *this;
+	}
+	bool operator != (const BSTIterator & rhs) const
+	{
+		return rhs.masterTree != this->masterTree;
+	}
+	bool operator == (const BSTIterator & rhs) const
+	{
+		return rhs.masterTree == this->masterTree;
+	}
+	T & operator * ()
+	{
+		return masterTree->data;
+	}
+	BSTIterator <T> & operator -- ()
+	{
+		//masterTree = masterTree->pPrev;
+		return *this;
+	}
+	BSTIterator <T> operator--(int prefix)
+	{
+		BSTIterator tmp(*this);
+		//masterTree = masterTree->pPrev;
+		return tmp;
+	}
+	BSTIterator <T> & operator ++ ()
+	{
+		//masterTree = masterTree->pNext;
+		return *this;
+	}
+	BSTIterator <T> operator++(int postfix)
+	{
+		BSTIterator tmp(*this);
+		//masterTree = masterTree->pNext;
+		return tmp;
+	}
+	template <class TT>
+	friend class BST;
+};
+
+
 #endif
