e93aed216ab1fc4a1ee792092d4afa991e3a71f5
Clint Goodman <goodmancf@ldschurch.org>
2015-03-02 18:45:07 -0700

    Adding bnode and huffman examples

---
+++ implementations/bnode/huffman.cpp
@@ -0,0 +1,151 @@
+/***********************************************************************
+ * Module:
+ *    Lesson 07, Huffman
+ *    Brother Helfrich, CS 235
+ * Author:
+ *    <your name>
+ * Summary:
+ *    This program will implement the huffman() function
+ ************************************************************************/
+
+#include "huffman.h"       // for HUFFMAN() prototype
+#include "pair.h"
+#include "vector.h"
+#include <cassert>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include "bnode.h"     // your BinaryNode class should be in bnode.h
+
+#include <fstream>         // needed for all file I/O programs
+using namespace std;
+/*******************************************
+ * HUFFMAN
+ * Driver program to exercise the huffman generation code
+ *******************************************/
+
+string findInTree(BinaryNode< Pair<string, float> > * pTree, string searchedFor, string encode);
+int getSizeOfTree(BinaryNode< Pair<string, float> > * pTree, int count);
+
+void huffman()
+{
+   char fileName[256];
+   //create the tree
+   Vector < Pair<string, float> > huffCodes;
+
+   //get the filename
+   cout << "Enter the filename containing the value frequencies: ";
+   cin >> fileName;
+
+   string line;
+   ifstream myfile(fileName);
+   if (myfile.is_open())
+   {
+	   while (getline(myfile, line))
+	   {
+		   stringstream  linestream(line);
+		   Pair<string, float> thisPair; //String: token, float: frequency
+		   linestream >> thisPair;
+		   huffCodes.push_back(thisPair);
+	   }
+	   myfile.close();
+   }
+   else
+   {
+	   cout << "Error opening file";
+   }
+
+   HuffmanTree master;
+   Vector <HuffmanTree> dTreesUnused;
+   for (int i = 0; i < huffCodes.size(); ++i)
+   {
+	   dTreesUnused.push_back(HuffmanTree(huffCodes[i]));
+   }
+   while(dTreesUnused.size() > 1)
+   {
+	   HuffmanTree * lowest = new  HuffmanTree();
+	   lowest->miniTree.data = Pair<string, float>("", 10000);
+	   HuffmanTree * secondLowest = new HuffmanTree();
+	   secondLowest->miniTree.data = Pair<string, float>("", 10001);
+	   VectorIterator <HuffmanTree> delOne;
+	   VectorIterator <HuffmanTree> delTwo;
+	   for (VectorIterator <HuffmanTree> it = dTreesUnused.begin(); it != dTreesUnused.end(); ++it)
+	   {
+		   if ((*it).miniTree.data.getSecond() < lowest->miniTree.data.getSecond())
+		   {
+				*secondLowest = *lowest;
+				delTwo = delOne;
+				*lowest = *it;
+				delOne = VectorIterator <HuffmanTree>(it);
+		   }
+		   else if ((*it).miniTree.data.getSecond() < secondLowest->miniTree.data.getSecond())
+		   {
+			   *secondLowest = *it;
+			   delTwo = VectorIterator <HuffmanTree> (it);
+		   }
+	   }
+	   HuffmanTree * combined = new HuffmanTree();
+	   combined->miniTree.addLeft(&lowest->miniTree);
+	   combined->miniTree.addRight(&secondLowest->miniTree);
+	   combined->miniTree.data = Pair<string, float>("", lowest->miniTree.data.getSecond() + secondLowest->miniTree.data.getSecond());
+
+	   (*delOne) = *combined;
+	   (*delTwo) = (*(--(dTreesUnused.end())));
+	   dTreesUnused.erase(dTreesUnused.end());
+   }
+   master = dTreesUnused[0];
+
+   for (VectorIterator < Pair<string, float> > it = huffCodes.begin(); it != huffCodes.end(); ++it)
+	{
+		cout << (*it).getFirst() << " = " << findInTree(&(master.miniTree), (*it).getFirst(), "") << endl;
+	}
+   return;
+}
+
+
+string findInTree(BinaryNode< Pair<string, float> > * pTree, string searchedFor, string encode)
+{
+	string toReturn = "";
+	BinaryNode < Pair<string, float> > * pLeft = pTree->pLeft;
+	BinaryNode < Pair<string, float> > * pRight = pTree->pRight;
+	if (pRight)
+	{
+		if (pRight->data.getFirst() == searchedFor)
+		{
+			return encode + string("1");
+		}
+		toReturn += findInTree(pRight, searchedFor, encode + "1");
+	}
+	if (pLeft)
+	{
+		if (pLeft->data.getFirst() == searchedFor)
+		{
+			return encode + string("0");
+		}
+		toReturn += findInTree(pLeft, searchedFor, encode + "0");
+	}
+
+	return toReturn;
+}
+
+int getSizeOfTree(BinaryNode< Pair<string, float> > * pTree, int count)
+{
+	int toReturn = 0;
+	BinaryNode < Pair<string, float> > * pLeft = pTree->pLeft;
+	BinaryNode < Pair<string, float> > * pRight = pTree->pRight;
+	if (pRight)
+	{
+		toReturn += getSizeOfTree(pRight, count + 1);
+	}
+	if (pLeft)
+	{
+		toReturn += getSizeOfTree(pLeft, count + 1);
+	}
+	if (!pLeft && !pRight)
+	{
+		return count;
+	}
+
+	return toReturn;
+}
---
+++ implementations/bnode/vector.h
@@ -0,0 +1,174 @@
+/***********************************************************************
+* Program:
+*    Lesson 00, Vector
+*    Brother Sloan, CS 235
+* Author:
+*    Clint Goodman
+* Summary:
+*    This is a header file that implements a data structure acting
+*	just like a vector.  
+************************************************************************/
+
+#include <iostream>       // for CIN and COUT
+
+// declare prototype
+template <class T>
+class VectorIterator;
+
+
+template <class T>
+class Vector
+{
+public:
+	//Default constructor
+	Vector() : data(NULL), current_size(0), capacity_size(0)
+	{
+		Vector(0);
+	}
+	//Non-default constructor
+	Vector(int capacity_size) : data(NULL), current_size(0), capacity_size(capacity_size) {}
+	~Vector()  //Destructor
+	{
+		data = NULL;
+		delete [] data;
+	}
+	//Copy constructor
+	Vector(const Vector<T> & rhs)
+	{
+		capacity_size = rhs.capacity_size;
+		current_size = rhs.current_size;
+		data = new T[capacity_size];
+		for (int i = 0; i < rhs.current_size; i++)
+		{
+			this->data[i] = rhs.data[i];
+		}
+	}
+	bool empty()
+	{
+		return (current_size == 0);
+	}
+	int size()
+	{
+		return current_size;
+	}
+	int capacity()
+	{
+		return capacity_size;
+	}
+	void clear()
+	{
+		data = NULL;
+		current_size = 0;
+	}
+	void push_back(T item)
+	{
+		//Capacity has to be more than one
+		if (data == NULL)
+		{
+			initFromZero();
+		}
+		//If the capactiy is full, double the capactiy
+		if (current_size >= capacity_size)
+		{
+			extendSize();
+		}
+		//Add the data
+		data[current_size] = item;
+		current_size++;
+	}
+	T & operator [](int index) throw(const char *)
+	{
+		//Check the range of the index
+		if (index < 0 || index >= current_size)
+		{
+			throw "ERROR: Invalid index";
+		}
+		return data[index];
+	}
+	VectorIterator <T> begin()
+	{
+		return VectorIterator <T>(data);
+	}
+	VectorIterator <T> end()
+	{
+		return VectorIterator <T>(data + current_size);
+	}
+	void erase(VectorIterator <T> toErase)
+	{
+		if (toErase != this->end())
+		{
+			(*toErase) = this->data[current_size - 1];
+		}
+		else if (toErase != this->begin())
+		{
+			(*toErase) = this->data[current_size];
+		}
+		current_size--;
+		return;
+	}
+
+private:
+	T * data;
+	int current_size;
+	int capacity_size;
+	void initFromZero()
+	{
+		if (capacity_size == 0)
+		{
+			capacity_size = 1;
+		}
+		data = new T[capacity_size];
+	}
+	void extendSize()
+	{
+		capacity_size *= 2;
+		*this = Vector(*this);
+	}
+};
+
+template <class T>
+class VectorIterator
+{
+public:
+	VectorIterator() : pVector(NULL) {}
+	VectorIterator(T * pVectorInput) : pVector(pVectorInput) {}
+	VectorIterator(const VectorIterator & rhs)  { *this = rhs; }
+	bool operator != (const VectorIterator & rhs) const
+	{
+		return rhs.pVector != this->pVector;
+	}
+
+	bool operator == (const VectorIterator & rhs) const
+	{
+		return rhs.pVector == this->pVector;
+	}
+	
+	VectorIterator & operator = (const VectorIterator & rhs)
+	{
+		this->pVector = rhs.pVector;
+		return *this;
+	}
+	T & operator * ()
+	{
+		return *pVector;
+	}
+	VectorIterator<T> & operator ++ ()
+	{
+		pVector++;
+		return *this;
+	}
+	VectorIterator<T> & operator -- ()
+	{
+		pVector--;
+		return *this;
+	}
+	VectorIterator<T> & operator ++ (int postfix)
+	{
+		VectorIterator tmp(*this);
+		pVector++;
+		return tmp;
+	}
+private:
+	//Pointer of generic type
+	T * pVector;
+};
