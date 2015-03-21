37d3297a3b89d83766f2188b9fc69958a396f265
Clint Goodman <goodmancf@ldschurch.org>
2015-03-21 15:42:03 -0600

    Adding initial files

---
+++ Hash/Hash/hash.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+ * Program:
+ *    Lesson 11, Hash
+ *    Brother Sloan, CS 235
+ * Author:
+ *      Clint Goodman and Parker Hubbard
+ * Summary:
+ ************************************************************************/
+#ifndef HASH_H
+#define HASH_H
+
+#include <cassert>
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**********************************************************************
+ * Hash
+ ***********************************************************************/
+template <class T>
+class Hash
+{
+};
+
+#endif
