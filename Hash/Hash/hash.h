dbfebf00e62c174ea71354d85947e45d15122bd1
Clint Goodman <goodmancf@ldschurch.org>
2015-03-23 20:32:28 -0600

    Stubbing out skeleton

--- Hash/Hash/hash.h
+++ Hash/Hash/hash.h
@@ -20,6 +20,60 @@ using namespace std;
 template <class T>
 class Hash
 {
+public:
+	Hash()
+	{
+
+	}
+
+	Hash(int buckets)
+	{
+
+	}
+
+	~Hash()
+	{
+
+	}
+
+	Hash(const Hash & rhs)
+	{
+
+	}
+
+	bool empty() const
+	{
+		return (sizeInt == 0);
+	}
+
+	int size() const
+	{
+		return sizeInt;
+	}
+
+	int capacity() const
+	{
+		return sizeInt;
+	}
+
+	//The parameter is the value to be found. Returns true if the value is found, false otherwise.
+	bool find() const
+	{
+		return false;
+	}
+
+	//Places a new instance of a value in the Hash. The parameter is the item to be inserted; there is no return value.
+	bool insert() const
+	{
+		sizeInt++;
+		return false;
+	}
+
+	//This is a pure virtual function taking an element as a parameter and returning an index into the underlying array.
+	virtual int hash(const T & value) const = 0;
+
+private:
+	int sizeInt;
 };
 
 #endif
