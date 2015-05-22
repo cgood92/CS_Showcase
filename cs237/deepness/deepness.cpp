ea4a4daf406dd0ba0939b104f2663854d5027084
Clint Goodman <goodmancf@ldschurch.org>
2015-05-22 10:38:13 -0600

    Adding initial files

---
+++ cs237/deepness/deepness.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+* What to do, what to do ...
+*************************************************************************/
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/*************************************************************************
+ * learned
+ *
+ * Tells interested parties what you learned.
+ *
+ * Use one or more calls to out(...), or else see the whatIlearned
+ * script and do the following:
+ *
+ *   Words words = {
+ *   #include "whatIlearned.inc"
+ *   nl};
+ *   cout << words;
+ *************************************************************************/
+void learned()
+{
+}
+
+/*************************************************************************
+ * usage
+ *
+ * Tells the user how to use the program.
+ *************************************************************************/
+void usage(const char * programName)
+{
+}
+
+/***************************************************************
+ * Runs one test, identified by its string argument.
+ ***************************************************************/
+void runOne(string test)
+{
+   cout << "\nRunning test " << test << endl;
+}
+
+/***************************************************************
+ * Runs all tests.
+ ***************************************************************/
+void runAll()
+{
+   cout << "Running all tests.\n";
+}
